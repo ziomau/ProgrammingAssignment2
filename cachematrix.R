10a334d15773aaed582700d93167f2c21d522bcd
ziomau <ziomau@users.noreply.github.com>
2015-08-19 11:21:12 +0200

    Update cachematrix.R
    Stub file updated with actual code for assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,127 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera "R Programming" course (rprog-031), Assignment #2, August 2015
+##
+## The purpose of the following R code is to cache the inverse of a matrix
+## so to avoid unnecessary time-consuming computations.
+## The code leverages R lexical scoping, and is inspired by the
+## "Caching the Mean of a Vector" referece example given in the course.
+##
+## The code contains three functions:
+##  'makeCacheMatrix()': Creates a special data structure supporting matrix caching.
+##  'cacheSolve()': Calculates the inverse of a matrix by exploiting caching.
+##  'testCacheSolve()': a simple function for testing purposes.
 
-## Write a short comment describing this function
+## The 'makeCacheMatrix()' function creates a special "matrix",
+## which is really a list containing proper local functions to:
+##  'set()': Set the data of the matrix.
+##  'get()': Get the data of the matrix.
+##  'setinverse()': Set the data of the inverse of the matrix.
+##  'getinverse()': Get the data of the inverse of the matrix.
+## The function has one input parameter that is the matrix to be stored,
+## and whose inverse has to be cached.
+makeCacheMatrix <- function(m = matrix()) {
+  # Creates the variable for storing the inverse of the matrix
+  # and initializes it to the NULL value.
+  i <- NULL
 
-makeCacheMatrix <- function(x = matrix()) {
+  # 'set()': Local function to set the data of the matrix to be stored.
+  # It has one input parameter, that is the matrix to be set.
+  set <- function(y) {
+    # Assigns the matrix passed as the input parameter to the matrix stored
+    # by this special data structure. Please note the use of the scoping
+    # assignment operator so to leverage the lexical scoping features.
+    m <<- y
 
+    # "Clears" the inverse cached matrix since a new matrix has just been set.
+    i <<- NULL
+  }
+
+  # 'get()': Local function to get the data of the matrix stored.
+  get <- function() {
+    # Returns the matrix stored in this special data structure.
+    m
+  }
+
+  # 'setinverse()': Local function to set the data of the inverse of the matrix.
+  # It has one input parameter, that is the inverse of the matrix to be set.
+  setinverse <- function(z) {
+    # Assigns the matrix passed as the input parameter to the inverse of the matrix
+    # stored by this special data structure. Again, please note the use of the
+    # scoping assignment operator so to leverage the lexical scoping features.
+    i <<- z
+  }
+
+  # 'getinverse()': Local function to get the data of the inverse of the matrix stored.
+  getinverse <- function() {
+    # Returns the inverse of matrix stored in this special data structure.
+    i
+  }
+
+  # The special data structure, i.e., a list referencing the local functions
+  # defined above.
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The 'cacheSolve()' function returns a matrix that is the inverse of the "matrix"
+## passed as input parameter. The function uses the 'solve()' function to calculate
+## the inverse of the matrix. The function leverages the special data structure and
+## the related functions defined in the 'makeCacheMatrix()' function in order to
+## avoid unnecessary computations when possible, i.e., when cached data is available.
+## The input parameter is a "special matrix", i.e., the special data structure
+## created by using the makeCacheMatrix()' function.
+cacheSolve <- function(sm, ...) {
+  # Gets the cached inverse of the matrix from the data structure passed as input.
+  im <- sm$getinverse()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Checks if the cached inverse matrix is not NULL, i.e., cached data is available.
+  if(!is.null(im)) {
+    # In such case, prints a message and returns the cached inverse matrix.
+    message("Getting cached data of the inverse of the matrix.")
+    return(im)
+  }
+
+  # Otherwise, no cached data is available.
+  # Therefore, retrieves the matrix data stored in the special data structure
+  # by using the 'get()' local function.
+  matrixData <- sm$get()
+  # Calculates the inverse of the matrix stored.
+  inverseMatrix <- solve(matrixData, ...)
+  # Stores the calculated inverse of the matrix in the special data structure
+  # by using the 'setinverse()' local function.
+  sm$setinverse(inverseMatrix)
+  # Prints a message to the user.
+  message("Calculating data of the inverse of the matrix.")
+  # Returns the calculated inverse of the matrix.
+  return(inverseMatrix)
+}
+
+## Simple function for testing purposes only.
+## It is not part of the course assignment.
+testCacheSolve <- function() {
+  # Creates a simple invertible matrix and prints it.
+  # testMatrix <- rbind(c(4, 3), c(3, 2))
+  testMatrix <- rbind(c(1, 3, 3), c(1, 4, 3), c(1, 3, 4))
+  message("Test matrix:")
+  print(testMatrix)
+  readline(prompt="Press [enter] to continue")
+
+  # Initializes the special data structure with the test matrix.
+  cacheMatrix <- makeCacheMatrix(testMatrix)
+
+  # Calculates the inverse of the test matrix and prints the results.
+  # In this case, the "Calculating data" message should be printed because
+  # this is the first time the inverse is calculated.
+  temp <- cacheSolve(cacheMatrix)
+  print(temp)
+  readline(prompt="Press [enter] to continue")
+
+  # Calculates again the inverse of the test matrix and prints the results.
+  # In this case, the "Getting cached data" message should be printed because
+  # this is the second time the inverse is calculated on the same matrix,
+  # therefore cached data should be available.
+  temp <- cacheSolve(cacheMatrix)
+  print(temp)
 }
